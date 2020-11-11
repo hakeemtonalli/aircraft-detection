b8854c67a690bfb6cf2152f027035d06a55e29cb
hakeemtfrank <hakeemtfrank@gmail.com>
2020-11-10 20:36:35 -0800

    removed timer

--- run_batch.sh
+++ run_batch.sh
@@ -1,31 +1,17 @@
-# run with rational quadratic Kernel
+
 echo "Beginning batch process"
 
-start = `date + %s`
+# run with rational quadratic Kernel
 python run.py --kernel="RQ_Kernel"
-end = `date + %s`
-echo $((end - start))
 
 # run with rbf kernel
-start = `date + %s`
 python run.py --kernel="RBF_Kernel"
-end = `date + %s`
-echo $((end - start))
 
 # run with linear kernel
-start = `date + %s`
 python run.py --kernel="Linear_Kernel"
-end = `date + %s`
-echo $((end - start))
 
 # run with quadratic kernel
-start = `date + %s`
 python run.py --kernel="Quadratic_Kernel"
-end = `date + %s`
-echo $((end - start))
 
 # run with cubic kernel
-start = `date + %s`
 python run.py --kernel="Cubic_Kernel"
-end = `date + %s`
-echo $((end - start))
